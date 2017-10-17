c7a4d906576a59cb833167b9ccef03724fdaeca8
Michael Riess <mikeriess@gmail.com>
2017-10-17 13:42:20 -0700

    Fix spacing

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,50 +3,50 @@
 var PriorityQueue = require('../lib/priority-queue.js');
 
 module.exports = (logSources, printer) => {
-	var priorityDateAsc = (a, b) => a.date < b.date;
-	var queue = new PriorityQueue(priorityDateAsc);
+  var priorityDateAsc = (a, b) => a.date < b.date;
+  var queue = new PriorityQueue(priorityDateAsc);
 
-	// initial count of undrained sources
-	var numSources = logSources.length;
+  // initial count of undrained sources
+  var numSources = logSources.length;
 
-	/**
-	 * The initial inserts can happen in any order since the queue will
-	 * prioritize the items, but have to wait until all initial inserts
-	 * are finished to start popping and printing.
-	 */
-	var populateQueue = Promise.all(logSources.map((source, sourceIndex) => {
-		return source.popAsync().then(log => {
-			var drained = log === false;
+  /**
+   * The initial inserts can happen in any order since the queue will
+   * prioritize the items, but have to wait until all initial inserts
+   * are finished to start popping and printing.
+   */
+  var populateQueue = Promise.all(logSources.map((source, sourceIndex) => {
+    return source.popAsync().then(log => {
+      var drained = log === false;
 
-			drained ? numSources-- : queue.insert(log, sourceIndex);
-		})
-	}));
+      drained ? numSources-- : queue.insert(log, sourceIndex);
+    })
+  }));
 
-	/**
-	 * Chain a new promise to the lastPromise with the next pop/insert
-	 * operation until no undrained sources remain.
-	 */
-	function popAndInsertAsync(lastPromise) {
-		lastPromise.then(() => {
-			if (numSources > 0) {
-				var nextLogOut = queue.pop();
+  /**
+   * Chain a new promise to the lastPromise with the next pop/insert
+   * operation until no undrained sources remain.
+   */
+  function popAndInsertAsync(lastPromise) {
+    lastPromise.then(() => {
+      if (numSources > 0) {
+        var nextLogOut = queue.pop();
 
-				printer.print(nextLogOut.value);
+        printer.print(nextLogOut.value);
 
-				var replacementLogIndex = nextLogOut.sourceIndex;
+        var replacementLogIndex = nextLogOut.sourceIndex;
 
-				var nextPromise = logSources[replacementLogIndex].popAsync().then(replacementLog => {
-					var drained = replacementLog === false;
+        var nextPromise = logSources[replacementLogIndex].popAsync().then(replacementLog => {
+          var drained = replacementLog === false;
 
-					drained ? numSources-- : queue.insert(replacementLog, replacementLogIndex);
-				});
+          drained ? numSources-- : queue.insert(replacementLog, replacementLogIndex);
+        });
 
-				popAndInsertAsync(nextPromise);
-			} else {
-				printer.done();
-			}
-		});
-	}
+        popAndInsertAsync(nextPromise);
+      } else {
+        printer.done();
+      }
+    });
+  }
 
-	popAndInsertAsync(populateQueue);
+  popAndInsertAsync(populateQueue);
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,32 +3,32 @@
 var PriorityQueue = require('../lib/priority-queue.js');
 
 module.exports = (logSources, printer) => {
-	var priorityDateAsc = (a, b) => a.date < b.date;
-	var queue = new PriorityQueue(priorityDateAsc);
+  var priorityDateAsc = (a, b) => a.date < b.date;
+  var queue = new PriorityQueue(priorityDateAsc);
 
-	// initial count of undrained sources
-	var numSources = logSources.length;
+  // initial count of undrained sources
+  var numSources = logSources.length;
 
-	// populate the queue with a log from each source
-	for (var sourceIndex = 0; sourceIndex < numSources; sourceIndex++) {
-		var log = logSources[sourceIndex].pop();
-		// a source isn't guaranteed to have logs
-		var drained = log === false;
+  // populate the queue with a log from each source
+  for (var sourceIndex = 0; sourceIndex < numSources; sourceIndex++) {
+    var log = logSources[sourceIndex].pop();
+    // a source isn't guaranteed to have logs
+    var drained = log === false;
 
-		drained ? numSources-- : queue.insert(log, sourceIndex);
-	}
+    drained ? numSources-- : queue.insert(log, sourceIndex);
+  }
 
-	while (numSources > 0) {
-		var nextLogOut = queue.pop();
-		printer.print(nextLogOut.value);
+  while (numSources > 0) {
+    var nextLogOut = queue.pop();
+    printer.print(nextLogOut.value);
 
-		// replace the popped log with one from the same source
-		var replacementLogSourceIndex = nextLogOut.sourceIndex;
-		var replacementLog = logSources[replacementLogSourceIndex].pop();
-		var replacementLogSourceDrained = replacementLog === false;
+    // replace the popped log with one from the same source
+    var replacementLogSourceIndex = nextLogOut.sourceIndex;
+    var replacementLog = logSources[replacementLogSourceIndex].pop();
+    var replacementLogSourceDrained = replacementLog === false;
 
-		replacementLogSourceDrained ? numSources-- : queue.insert(replacementLog, replacementLogSourceIndex);
-	}
+    replacementLogSourceDrained ? numSources-- : queue.insert(replacementLog, replacementLogSourceIndex);
+  }
 
-	printer.done();
+  printer.done();
 }
